55dcb108f7240a3f66e43455b587c289a83d8455
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-07 17:38:47 +0700

    Pengguna memasukkan elemen dan kemudian  menampilan array yang tersusun

--- BubbkeShort_C/Program.cs
+++ BubbkeShort_C/Program.cs
@@ -6,7 +6,7 @@ namespace BubbleShort_C
     {
         // Deklarasi array int dengan ukuran 20
         private int[] a = new int[20];
-        
+
         // Deklarasi variabnel int untuk menyimpan banyak data pada array
 
         // Fungsi / Method untuk menerima masukan
@@ -15,12 +15,35 @@ namespace BubbleShort_C
             // menerima angka untuk menentukan banyaknya data yang disimpan pada array
             while (true)
             {
-                Console.Write(a"Masukan banyaknya element pada array: ");
+                Console.Write("Masukan banyaknya element pada array: ");
                 string s = Console.ReadLine();
                 new = Int32.Parse(s);
-                if (new <= 20)
+                if (n <= 20)
                     break;
                 else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
             }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Masukkan elemen array");
+            Console.WriteLine("-----------------------");
+
+            // Pengguna memasukkan elemen pada array
+            for (int i = 0; i < new; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+
+            }
+        }
+        public void display()
+        {
+            // Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Element Array yang telah tersusun");
+            Console.WriteLine("---------------------------------");
+            for (int j =)
         }
     }
